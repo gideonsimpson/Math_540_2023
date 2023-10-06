e307e0cd737b21096d2bfab377cbae96d5609001
Gideon Simpson <gideon.simpson@gmail.com>
2023-10-05 20:44:35 -0400

    added timing example

---
+++ lectures/bvps1d.jl
@@ -0,0 +1,22 @@
+
+function assemble_matrix(n, Δx)
+    A = zeros(n - 1, n - 1)
+
+    for i = 1:n-1
+        # treat the first and last rows seperately
+        if i == 1
+            A[1, 1] = 2 / Δx^2
+            A[1, 2] = -1 / Δx^2
+        elseif i == n - 1
+            A[n-1, n-2] = -1 / Δx^2
+            A[n-1, n-1] = 2 / Δx^2
+        else
+            A[i, i-1] = -1 / Δx^2
+            A[i, i] = 2 / Δx^2
+            A[i, i+1] = -1 / Δx^2
+        end
+
+    end
+
+    return A
+end
